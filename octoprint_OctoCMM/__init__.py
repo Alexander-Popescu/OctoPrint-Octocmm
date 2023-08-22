78b57b87382088ec5011069788ec4809e0ba4f41
Alexander Popescu <AlexanderPopescu5381@gmail.com>
2023-08-22 17:26:57 -0400

    added settings page where plugin variables are editable

--- octoprint_OctoCMM/__init__.py
+++ octoprint_OctoCMM/__init__.py
@@ -1,9 +1,21 @@
 import octoprint.plugin
 
 class OctoCMMPlugin(octoprint.plugin.StartupPlugin,
-                    octoprint.plugin.TemplatePlugin):
+                    octoprint.plugin.TemplatePlugin,
+                    octoprint.plugin.SettingsPlugin):
     def on_after_startup(self):
-        self._logger.info("Console Log from OctoCMM plugin!")
+        #runs after plugin startup
+        self._logger.info("OctoCMM Plugin! (URL: %s)" % self._settings.get(["url"]))
+
+    def get_settings_defaults(self):
+        #this is where you can define default setting values
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+    
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False)
+        ]
 
 __plugin_name__ = "OctoCMM"
 __plugin_pythoncompat__ = ">=3.7,<4"
