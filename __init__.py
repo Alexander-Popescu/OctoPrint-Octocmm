4bd45a7b253e7c736475083c3491d2cf80235489
sebi popescu <Dogelover5381@gmail.com>
2023-08-21 20:18:07 -0400

    minor changes

--- __init__.py
+++ __init__.py
@@ -9,8 +9,6 @@ class OctoCMMPlugin(octoprint.plugin.StartupPlugin):
         self._logger.info("Log Test from OctoCMM Plugin!")
 
 __plugin_name__ = "OctoCMM"
-__plugin_version__ = "1.0.0"
-__plugin_description__ = "A plugin to use a 3d printer gantry as a CMM"
 __plugin_pythoncompat__ = ">=3.7,<4"
 #tells octoprint to instantiate the cmm class
 __plugin_implementation__ = OctoCMMPlugin()
